8e0eddbea18b2ccf01f4ae7448b9711991a7bdc8
piano <syedmutahir908@gmail.com>
2024-08-13 10:05:46 -0700

    done hogaya

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
 
 function Home() {
-//   const apiUrl = "https://you904.vercel.app/api";
+  const apiUrl = "https://you904.vercel.app/api";
 const [mess ,setMess]= useState('')
-// fetch(`${apiUrl}/endpoint`)
-//   .then(response => response.json())
-//   .then(data => {
-//     setMess(data.message); // Set the message from the backend
-//     console.log(data);
-//   })
-//   .catch(error => console.error('Error:', error));
+fetch(`${apiUrl}/endpoint`)
+  .then(response => response.json())
+  .then(data => {
+    setMess(data.message); // Set the message from the backend
+    console.log(data);
+  })
+  .catch(error => console.error('Error:', error));
 
   return (
     <div>home {mess}</div>
